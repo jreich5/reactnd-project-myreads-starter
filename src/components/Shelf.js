4534eccce8c8d159c03e287079b42493e8709f91
Justin Reich <justinreich.dev@gmail.com>
2019-09-13 10:23:05 -0500

    feat: Add Shelf component

--- src/components/Shelf.js
+++ src/components/Shelf.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import BookList from './BookList'
 
 class Shelf extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.shelfTitle}</h2>
+        <BookList books={this.props.books} setShelf={this.props.setShelf} />
       </div>
     );
   }
 }
 
 Shelf.propTypes = {
-
+  books: PropTypes.array.isRequired,
+  setShelf: PropTypes.func.isRequired,
+  shelfTitle: PropTypes.string.isRequired
 };
 
 export default Shelf;
