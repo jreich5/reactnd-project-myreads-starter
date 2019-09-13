9ccdc679e27090a9cd274c4b946c540f3f1530f0
Justin Reich <justinreich.dev@gmail.com>
2019-09-13 10:22:40 -0500

    feat: Add ShelfList component

--- src/components/ShelfList.js
+++ src/components/ShelfList.js
@@ -1,18 +1,32 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Shelf from './Shelf'
 
 class ShelfList extends Component {
+
+  getShelfBooks = (shelfProperty, books) => {
+    return books.filter(book => book.shelf === shelfProperty);
+  }
+ 
   render() {
     return (
-      <div>
-        
+      <div className="list-books-content">
+        <div>
+          {this.props.shelfs.map(shelf => <Shelf 
+            key={shelf.shelfProperty} 
+            shelfTitle={shelf.shelfTitle} 
+            books={this.getShelfBooks(shelf.shelfProperty, this.props.books)} 
+            setShelf={this.props.setShelf} />)}
+        </div>
       </div>
     );
   }
 }
 
 ShelfList.propTypes = {
-
+  books: PropTypes.array.isRequired,
+  setShelf: PropTypes.func.isRequired,
+  shelfs: PropTypes.array.isRequired
 };
 
 export default ShelfList;
