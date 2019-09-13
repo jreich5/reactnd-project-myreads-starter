dd13ce11c7f2b6a1d9e849b28b1653f1f4441145
Justin Reich <justinreich.dev@gmail.com>
2019-09-13 10:23:26 -0500

    feat: Add BookList component

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,18 +1,22 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Book from './Book'
 
 class BookList extends Component {
   render() {
     return (
-      <div>
-        
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+          {this.props.books.map(book => <Book key={book.id} book={book} setShelf={this.props.setShelf} />)}
+        </ol>
       </div>
     );
   }
 }
 
 BookList.propTypes = {
-
+  books: PropTypes.array.isRequired,
+  setShelf: PropTypes.func.isRequired
 };
 
 export default BookList;
