9fa0a49b6da26adb5be708fc668fc39507645da0
Justin Reich <justinreich.dev@gmail.com>
2019-09-13 10:24:02 -0500

    feat: Add Book component

--- src/components/Book.js
+++ src/components/Book.js
@@ -4,17 +4,31 @@ import PropTypes from 'prop-types';
 class Book extends Component {
   render() {
     return (
-      <div>
-        
-      </div>
+      <li>
+        <div className="book">
+          <div className="book-top">
+            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+            <div className="book-shelf-changer">
+              <select>
+                <option value="move" disabled>Move to...</option>
+                <option value="currentlyReading">Currently Reading</option>
+                <option value="wantToRead">Want to Read</option>
+                <option value="read">Read</option>
+                <option value="none">None</option>
+              </select>
+            </div>
+          </div>
+          <div className="book-title">{this.props.book.title}</div>
+          <div className="book-authors">{this.props.book.authors}</div>
+        </div>
+      </li>
     );
   }
 }
 
 Book.propTypes = {
   book: PropTypes.object.isRequired,
-  currentShelf: PropTypes.string.isRequired,
-  
+  setShelf: PropTypes.func.isRequired
 };
 
 export default Book;
