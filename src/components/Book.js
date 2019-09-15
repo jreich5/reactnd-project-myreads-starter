9fb07d5447bd724e03d261f0708347674998edf9
Justin Reich <justinreich.dev@gmail.com>
2019-09-15 10:54:33 -0500

    fix: Edit Book component to have no background image if no thumbnail is provided

--- src/components/Book.js
+++ src/components/Book.js
@@ -26,7 +26,13 @@ class Book extends Component {
       <li>
         <div className="book">
           <div className="book-top">
-            <div className="book-cover" style={{ width: 128, height: 193, backgroundImage: `url(${this.props.book.imageLinks.thumbnail})` }}></div>
+            <div className="book-cover" 
+              style={{
+                 width: 128, 
+                 height: 193, 
+                 backgroundImage: (`url(${this.props.book.imageLinks ? this.props.book.imageLinks.thumbnail : 'none'}})`) 
+                }}
+            ></div>
             <div className="book-shelf-changer">
               <select value={this.state.shelfSelection} onChange={this.changeSelect}>
                 <option value="move" disabled>Move to...</option>
